9c742569d871fe00629448b891e01abc09fbe7e7
Amar Phule <amardeepphule@gmail.com>
2024-07-13 18:13:47 +0530

    activity-1 variable declaration

---
+++ 01-day.js
@@ -0,0 +1,17 @@
+// Day 1: Variables and Data Types
+
+// Task/Activities:
+
+// Activity 1: Variable declaration
+
+// Task 1: Declare a variable using `var`, assign it a number, and log the value to the console.
+
+var courses = 5;
+
+console.log("Courses:", courses);
+
+// Task 2: Declare a variable using `let`, assign it a string, and log the value to the console.
+
+var username = "Amardeep";
+
+console.log("Name:", username);
