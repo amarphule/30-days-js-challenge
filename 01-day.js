077764e2a446c045a47957fac545bc5808e01998
Amar Phule <amardeepphule@gmail.com>
2024-07-13 18:19:31 +0530

    Activity 2: Constant declaration

--- 01-day.js
+++ 01-day.js
@@ -11,3 +11,9 @@ console.log("Courses:", courses);
 // Task 2: Declare a variable using `let`, assign it a string, and log the value to the console.
 let username = "Amardeep";
 console.log("Name:", username);
+
+// Activity 2: Constant declaration
+// Task 3: Declare a variable using `const`, assign it a boolean, and log the value to the console.
+
+const isAdmin = true;
+console.log("Role :", isAdmin);
