7f6e19dff20e504c82235e5152576b2ce3e944e9
Amar Phule <amardeepphule@gmail.com>
2024-07-14 17:01:14 +0530

    Activity 1: Arithmatic Operators

---
+++ 02-day.js
@@ -0,0 +1,29 @@
+// Day 2: Operators
+
+// Activity 1: Arithmatic Operators
+
+function AddNumber(a, b) {
+  console.log(`Sum of ${a}, ${b}is: ${a + b}`);
+}
+
+function SubNumber(a, b) {
+  console.log(`Subtract of ${a}, ${b} is: ${a - b}`);
+}
+
+function MultipyNumber(a, b) {
+  console.log(`Multiplication of ${a}, ${b}is: ${a + b}`);
+}
+
+function DivideNumber(a, b) {
+  console.log(`Division of ${a}, ${b}is: ${a + b}`);
+}
+
+function FindRemainder(a, b) {
+  console.log(`Remainder of ${a}, ${b}is: ${a + b}`);
+}
+
+AddNumber(5, 6);
+SubNumber(10, 5);
+MultipyNumber(5 * 2);
+DivideNumber(12, 4);
+FindRemainder(89, 9);
